6a1234d3bdbb07a0572f3225083bc17890984603
ben <benfherm@gmail.com>
2020-12-08 07:04:07 -0800

    initial

---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'dart:io';
+import 'dart:developer' as developer;
+import 'package:camera_camera/page/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vonage/verify.dart';
+import 'dart:convert';
+import 'package:vonage/vonage.dart';
+
+Future<void> vonage() async {
+  var auth = VerifyVonage(
+      "043f9d65", "dMjl3Fft4CzTwoAk", "+972508676743", "brandName", 4);
+  print(auth);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // var auth = VerifyVonage("apiKey", "apiSecret", "phoneNumber", "brandName", 4);
+  // auth.then((res) {
+  //   print(res); //response from vonage in json format;
+  // }).catchError((err) {
+  //   print(err); // In case pf error
+  // });
+  await vonage();
+  print('hello');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Revilion Poc';
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    return Form(
+        key: _formKey,
+        child: Column(children: <Widget>[
+          // Container(
+          //   margin: EdgeInsets.symmetric(vertical: 20.0),
+          //   child: ,
+          // )
+          TextFormField(
+            decoration: new InputDecoration(labelText: "Enter your number"),
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ], // Only numbers can be entered
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              if (!regex.hasMatch(value)) {
+                return 'Please enter a valid email';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 15),
+          ElevatedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, otherwise false.
+              if (_formKey.currentState.validate()) {
+                // If the form is valid, display a snackbar. In the real world,
+                // you'd often call a server or save the information in a database.
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
+            },
+            child: Text('Submit'),
+          )
+          // Add TextFormFields and ElevatedButton here.
+        ]));
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File val;
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.camera_alt),
+          onPressed: () async {
+            val = await showDialog(
+                context: context,
+                builder: (context) => Camera(
+                      mode: CameraMode.fullscreen,
+                      initialCamera: CameraSide.front,
+                      enableCameraChange: true,
+                      orientationEnablePhoto: CameraOrientation.landscape,
+                      onChangeCamera: (direction, _) {
+                        print('--------------');
+                        print('$direction');
+                        print('--------------');
+                      },
+                      // imageMask: CameraFocus.square(
+                      //   color: Colors.black.withOpacity(0.5),
+                      // ),
+                    ));
+            setState(() {});
+          }),
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
+        // margin: const EdgeInsets.all(40.0),
+        // Center is a layout widget. It takrs a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MyCustomForm(),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
